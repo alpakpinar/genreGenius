afd09b81a32d9f060d1d5ddcaea33c57ef8b1e80
Alp Akpinar <aakpinar@bu.edu>
2019-11-20 11:13:32 -0500

    Executable file to run the whole processing job

--- word2Vec.py
+++ word2Vec.py
@@ -1,1 +1,15 @@
-### Experimenting with using word2Vec
+#!/usr/bin/env python
+
+from utils.data_processor import DataProcessor
+
+def main():
+    '''Execute the job.
+    Load and pre-process the lyrics,
+    get the song arrays and labels and
+    store them in pickle files.'''
+    processor = DataProcessor()
+    processor.dump_to_pkl()
+
+if __name__ == '__main__':
+    main()
+     
