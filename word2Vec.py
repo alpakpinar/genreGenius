8ab7f178c09713aeb22ccd0aedf48caec7f7759d
Alp Akpinar <aakpinar@bu.edu>
2019-11-26 16:47:02 -0500

    Add num_common_words command line option while executing

--- word2Vec.py
+++ word2Vec.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python
 
+import argparse
 from utils.data_processor import DataProcessor
 
 def main():
     '''Execute the job.
     Load and pre-process the lyrics,
     get the song arrays and labels and
-    store them in npy files.'''
+    store them in npy files.
+
+    If num_common_words option is specified,
+    num_common_words number of most common words 
+    will be removed from the lyrics.
+    '''
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--num_common_words', help='Number of most common words to be removed.', type=int)
+    args = parser.parse_args()
+
     processor = DataProcessor()
-    processor.dump_to_npy()
+    processor.dump_to_npy(num_common_words = args.num_common_words)
 
 if __name__ == '__main__':
     main()
